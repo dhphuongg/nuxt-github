bc1dfe06f75ff4ecd518b3ce9642a25c102e2999
dhphuongg <hoangphuong270703@gmail.com>
2025-01-01 00:07:10 +0700

    feat: enhance app layout with SEO metadata and add index page

--- app.vue
+++ app.vue
@@ -1,6 +1,40 @@
+<script setup lang="ts">
+useHead({ titleTemplate: 'Nuxt & Github' });
+</script>
+
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <Html lang="en">
+    <Head>
+      <Meta
+        property="og:title"
+        content="Nuxt & Github"
+      />
+      <Meta
+        property="og:site:name"
+        content="Nuxt & Github"
+      />
+      <Meta
+        property="og:description"
+        content="Nuxt & Github"
+      />
+      <Meta
+        name="description"
+        content="Nuxt & Github"
+      />
+      <Meta
+        name="google"
+        content="notranslate"
+      />
+      <Link
+        rel="icon"
+        href="/favicon.ico"
+      />
+    </Head>
+    <Body>
+      <NuxtLoadingIndicator />
+      <NuxtLayout>
+        <NuxtPage />
+      </NuxtLayout>
+    </Body>
+  </Html>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+const counter = ref<number>(0);
+</script>
+
+<template>
+  <h1>Nuxt + Github Action + Github Pages</h1>
+</template>
