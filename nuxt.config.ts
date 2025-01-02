885525ede6774b58341d967aadaf6b5c7ce856d7
dhphuongg <hoangphuong270703@gmail.com>
2025-01-02 13:50:23 +0700

    fix: enable baseURL configuration in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   app: {
-    // baseURL: '/nuxt-github',
+    baseURL: '/nuxt-github',
   },
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
